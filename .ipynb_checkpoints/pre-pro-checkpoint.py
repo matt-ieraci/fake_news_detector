dd674b3b9516929968aea3b6b54ce36a7c5ebc39
Maria Dryden <147217308+mariadryden@users.noreply.github.com>
2023-12-12 12:02:56 -0500

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/pre-pro-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing import sequence, Tokenizer
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense
-
-
